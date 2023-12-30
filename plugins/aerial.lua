3d767971fe4c4231a77f9a3a4b5983cf400a4f3b
Guy Kaplan <gkpln3@gmail.com>
2023-12-30 17:37:37 +0200

    Added stuff

--- plugins/aerial.lua
+++ plugins/aerial.lua
@@ -10,6 +10,17 @@ return {
     require("aerial").setup {
       -- Your configuration settings
       open_automatic = true, -- This setting will make Aerial open automatically
+      layout = {
+
+        default_direction = "right",
+
+        -- Determines where the aerial window will be opened
+        --   edge   - open aerial at the far right/left of the editor
+        --   window - open aerial to the right/left of the current window
+        placement = "edge",
+        preserve_equality = true,
+      },
+      attach_mode = "global",
     }
   end,
 }
